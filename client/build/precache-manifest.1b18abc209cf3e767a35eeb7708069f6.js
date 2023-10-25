d04aa7cc1554a6ff8a8676525b894e5c76f9df2b
UPADHYAY-YATENDRA <yupadhyayyk@gmail.com>
2023-10-25 17:05:11 +0530

    Removed build from gitignore

---
+++ client/build/precache-manifest.1b18abc209cf3e767a35eeb7708069f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a3cd90d752cf631e42e1b300db33118",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b853c7323267bef2500c",
+    "url": "/static/css/main.e67446cb.chunk.css"
+  },
+  {
+    "revision": "d5e34593e765a44fed05",
+    "url": "/static/js/2.141058fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.141058fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b853c7323267bef2500c",
+    "url": "/static/js/main.b69da13a.chunk.js"
+  },
+  {
+    "revision": "e04ae7cd41a349351b6c",
+    "url": "/static/js/runtime-main.995f955b.js"
+  }
+]);
